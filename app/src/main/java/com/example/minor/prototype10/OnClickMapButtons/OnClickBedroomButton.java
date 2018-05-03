39a8a81e5d050b4175f62f3c6289ae3eb2059930
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-05-04 03:18:17 +0900

    ベースキャンプ作成中

--- app/src/main/java/com/example/minor/prototype10/OnClickMapButtons/OnClickBedroomButton.java
+++ app/src/main/java/com/example/minor/prototype10/OnClickMapButtons/OnClickBedroomButton.java
@@ -23,6 +23,7 @@ public class OnClickBedroomButton extends SuperOnClickMapButton {
                 stopAllButtons();
                 int textNum;
                 textNum = (int)(Math.random()*8 + 1);
+                //ここはもっと増やしたい
                 switch (textNum){
                     case 1:
                         mainText.setText("その夜お前は奇妙な夢を見た...。\nおまえは走り続けていた。人には決して辿り着けないどこかを目指して...。");
@@ -37,7 +38,7 @@ public class OnClickBedroomButton extends SuperOnClickMapButton {
                         mainText.setText("その夜お前は奇妙な夢を見た...。\nおまえは認識できない何かの存在を恐れていた。この世界に存在する生命がそれを知ることは、決して許されることではなかった。");
                         break;
                     case 5:
-                        mainText.setText("何かにいざなわれるように、お前は深い眠りについた。");
+                        mainText.setText("お前は何かに誘われるように深い眠りについた。");
                         break;
                     case 6:
                         mainText.setText("家主はどんな人だったのだろう...。どうしてこんなところに住もうと思ったのだろう...。\nそんなことを考えているうちに意識が朦朧として、お前は眠りについた。");
@@ -46,7 +47,7 @@ public class OnClickBedroomButton extends SuperOnClickMapButton {
                         mainText.setText("お前はいつも劣っていた、しかしあの頃のお前には見守ってくれる存在もいたのかもしれない。\n過ぎ去った過去に思いをはせているうちに、お前は眠気に身を委ねていった。");
                         break;
                     case 8:
-                        mainText.setText("お前には何も残されていなかった。\nお前はこれからのことに考えを巡らせたが、何も得られるものはなかった。\nそうこうしているうちに、お前は眠りに誘われていった。");
+                        mainText.setText("お前には何も残されていなかった。\nお前はこれからのことに考えを巡らせたが、何も得られるものはなかった。\nそうこうしているうちにお前は眠りについた。");
                         break;
                 }
                 realm.beginTransaction();
@@ -58,7 +59,7 @@ public class OnClickBedroomButton extends SuperOnClickMapButton {
                     public void run() {
                         imageButton1Text.setText("眠る");
                         imageButton8Text.setText("戻る");
-                        mainText.setText("目覚めると傷が癒えていた。\n周囲を見渡して、お前はあることに気付く。\n\n時間が経っていない...。\n\n何度も目を疑ったが、お前は現実を受け入れるしかなかった。");
+                        mainText.setText("目覚めると傷が癒えていた。\n周囲を見渡して、お前はあることに気付く。\n\n時間が経っていない...。\n\n目を疑ったが、お前は現実を受け入れるしかなかった。");
                         startAllButtons();
                     }
                 }, 6500);
