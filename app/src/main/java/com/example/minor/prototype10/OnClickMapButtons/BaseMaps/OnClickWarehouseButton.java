99265c8d1b1bf652cee44ebd558567b1793775e9
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-07-18 07:42:43 +0900

    倉庫の基礎が完成しました。

--- app/src/main/java/com/example/minor/prototype10/OnClickMapButtons/BaseMaps/OnClickWarehouseButton.java
+++ app/src/main/java/com/example/minor/prototype10/OnClickMapButtons/BaseMaps/OnClickWarehouseButton.java
@@ -23,94 +23,6 @@ public class OnClickWarehouseButton extends SuperOnClickMapButton {
         mainText.setText("倉庫は薄暗く、様々なものが散らばっている。");
         MainActivity.soundPool.play(MainActivity.oldMansionWalkingSound, 1.0f, 1.0f, 1, 0, 1);
 
-        imageButton1Text.setText("武器の整理"); //追加
-        imageButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ValueAnimator animator = ValueAnimator.ofInt(0,180);
-                animator.setDuration(250);
-                animator.setInterpolator(new DecelerateInterpolator());
-                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                        backgroundImage.setImageAlpha((Integer) valueAnimator.getAnimatedValue());
-                    }
-                });
-                animator.start();
-                FragmentManager fragmentManager = mMain.getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                WarehouseWeaponFragment warehouseWeaponFragment = new WarehouseWeaponFragment();
-                fragmentTransaction.replace(R.id.warehouse_item_fragment, warehouseWeaponFragment);
-                fragmentTransaction.commit();
-            }
-        });
-
-        imageButton2Text.setText("防具の整理"); //追加
-        imageButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ValueAnimator animator = ValueAnimator.ofInt(0,180);
-                animator.setDuration(250);
-                animator.setInterpolator(new DecelerateInterpolator());
-                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                        backgroundImage.setImageAlpha((Integer) valueAnimator.getAnimatedValue());
-                    }
-                });
-                animator.start();
-                FragmentManager fragmentManager = mMain.getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                WarehouseArmorFragment warehouseArmorFragment = new WarehouseArmorFragment();
-                fragmentTransaction.replace(R.id.warehouse_item_fragment, warehouseArmorFragment);
-                fragmentTransaction.commit();
-            }
-        });
-
-        imageButton3Text.setText("回復薬の整理");
-        imageButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ValueAnimator animator = ValueAnimator.ofInt(0,180);
-                animator.setDuration(250);
-                animator.setInterpolator(new DecelerateInterpolator());
-                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                        backgroundImage.setImageAlpha((Integer) valueAnimator.getAnimatedValue());
-                    }
-                });
-                animator.start();
-                FragmentManager fragmentManager = mMain.getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                WarehouseRecoveryItemFragment warehouseRecoveryItemFragment = new WarehouseRecoveryItemFragment();
-                fragmentTransaction.replace(R.id.warehouse_item_fragment, warehouseRecoveryItemFragment);
-                fragmentTransaction.commit();
-            }
-        });
-
-        imageButton4Text.setText("その他の整理");
-        imageButton4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ValueAnimator animator = ValueAnimator.ofInt(0,180);
-                animator.setDuration(250);
-                animator.setInterpolator(new DecelerateInterpolator());
-                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                        backgroundImage.setImageAlpha((Integer) valueAnimator.getAnimatedValue());
-                    }
-                });
-                animator.start();
-                FragmentManager fragmentManager = mMain.getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                WarehouseOtherItemFragment warehouseOtherItemFragment = new WarehouseOtherItemFragment();
-                fragmentTransaction.replace(R.id.warehouse_item_fragment, warehouseOtherItemFragment);
-                fragmentTransaction.commit();
-            }
-        });
-
         OnClickOldMansion1FButton onClickOldMansion1FButton = new OnClickOldMansion1FButton();
         imageButton8.setOnClickListener(onClickOldMansion1FButton);
         imageButton8Text.setText("戻る");
@@ -130,9 +42,103 @@ public class OnClickWarehouseButton extends SuperOnClickMapButton {
                     playerInfo.setMoney(playerInfo.getMoney() + (int)(playerInfo.getIdoMoneyCount()*1.2));
                     playerInfo.setIdoMoneyCount(0);
                     realm.commitTransaction();
+                    resetAllButton();
                     mainText.setText("金だ！\nこれは今まで自分が井戸に投げ込んできたものではないか...\n気のせいか少し増えている気がする...");
-                    imageButton3.setOnClickListener(null);
-                    imageButton3Text.setText("");
+                    imageButton8Text.setText("おぉ...");
+                    imageButton8.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            createMap();
+                        }
+                    });
+                }
+            });
+        }else {
+            imageButton1Text.setText("武器の整理"); //追加
+            imageButton1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ValueAnimator animator = ValueAnimator.ofInt(0,180);
+                    animator.setDuration(250);
+                    animator.setInterpolator(new DecelerateInterpolator());
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                            backgroundImage.setImageAlpha((Integer) valueAnimator.getAnimatedValue());
+                        }
+                    });
+                    animator.start();
+                    FragmentManager fragmentManager = mMain.getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    WarehouseWeaponFragment warehouseWeaponFragment = new WarehouseWeaponFragment();
+                    fragmentTransaction.replace(R.id.warehouse_item_fragment, warehouseWeaponFragment);
+                    fragmentTransaction.commit();
+                }
+            });
+
+            imageButton2Text.setText("防具の整理"); //追加
+            imageButton2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ValueAnimator animator = ValueAnimator.ofInt(0,180);
+                    animator.setDuration(250);
+                    animator.setInterpolator(new DecelerateInterpolator());
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                            backgroundImage.setImageAlpha((Integer) valueAnimator.getAnimatedValue());
+                        }
+                    });
+                    animator.start();
+                    FragmentManager fragmentManager = mMain.getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    WarehouseArmorFragment warehouseArmorFragment = new WarehouseArmorFragment();
+                    fragmentTransaction.replace(R.id.warehouse_item_fragment, warehouseArmorFragment);
+                    fragmentTransaction.commit();
+                }
+            });
+
+            imageButton3Text.setText("回復薬の整理");
+            imageButton3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ValueAnimator animator = ValueAnimator.ofInt(0,180);
+                    animator.setDuration(250);
+                    animator.setInterpolator(new DecelerateInterpolator());
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                            backgroundImage.setImageAlpha((Integer) valueAnimator.getAnimatedValue());
+                        }
+                    });
+                    animator.start();
+                    FragmentManager fragmentManager = mMain.getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    WarehouseRecoveryItemFragment warehouseRecoveryItemFragment = new WarehouseRecoveryItemFragment();
+                    fragmentTransaction.replace(R.id.warehouse_item_fragment, warehouseRecoveryItemFragment);
+                    fragmentTransaction.commit();
+                }
+            });
+
+            imageButton4Text.setText("その他の整理");
+            imageButton4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ValueAnimator animator = ValueAnimator.ofInt(0,180);
+                    animator.setDuration(250);
+                    animator.setInterpolator(new DecelerateInterpolator());
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                            backgroundImage.setImageAlpha((Integer) valueAnimator.getAnimatedValue());
+                        }
+                    });
+                    animator.start();
+                    FragmentManager fragmentManager = mMain.getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    WarehouseOtherItemFragment warehouseOtherItemFragment = new WarehouseOtherItemFragment();
+                    fragmentTransaction.replace(R.id.warehouse_item_fragment, warehouseOtherItemFragment);
+                    fragmentTransaction.commit();
                 }
             });
         }
